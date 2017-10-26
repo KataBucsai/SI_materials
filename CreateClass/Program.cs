d47a8a9b25cb22c805609f07eef45f911ddb6b4a
KataBucsai <kata.kovacs@gmail.com>
2017-10-26 10:22:55 +0200

    implement Person class

---
+++ CreateClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person person = new Person("Joe", new DateTime(1990, 10, 10), Gender.Male);
+
+            Console.WriteLine(person.ToString());
+            
+        }
+    }
+}
