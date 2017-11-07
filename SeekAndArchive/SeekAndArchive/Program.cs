bf9dca900b10921f37718ac7e55087e43cccd0b1
KataBucsai <kata.kovacs@gmail.com>
2017-11-07 19:30:10 +0100

    implement delete and rename watcher

--- SeekAndArchive/SeekAndArchive/Program.cs
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -11,23 +11,31 @@ namespace SeekAndArchive
     {
         static List<FileInfo> FoundFiles;
         static List<FileSystemWatcher> watchers;
-
+        static private string fileName;
+        static private DirectoryInfo rootDir;
 
         static void Main(string[] args)
         {
-            string fileName = args[0];
+            fileName = args[0];
             string directoryName = args[1];
             FoundFiles = new List<FileInfo>();
             watchers = new List<FileSystemWatcher>();
 
             //examine if the given directory exists at all 
-            DirectoryInfo rootDir = new DirectoryInfo(directoryName);
+            rootDir = new DirectoryInfo(directoryName);
             if (!rootDir.Exists)
             {
                 Console.WriteLine("The specified directory does not exist.");
                 return;
             }
 
+            InitFilewatcher();
+
+        }
+
+        private static void InitFilewatcher()
+        {
+            FoundFiles.Clear();
             //search recursively for the mathing files
             RecursiveSearch(FoundFiles, fileName, rootDir);
             //list the found files
@@ -38,7 +46,6 @@ namespace SeekAndArchive
             }
 
             Console.ReadLine();
-
         }
 
         static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
@@ -48,6 +55,8 @@ namespace SeekAndArchive
                 foundFiles.Add(fil);
                 FileSystemWatcher newWatcher = new FileSystemWatcher(fil.DirectoryName, fil.Name);
                 newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
+                newWatcher.Deleted += new FileSystemEventHandler(WatcherDeleted);
+                newWatcher.Renamed += new RenamedEventHandler(WatcherRenamed);
                 newWatcher.EnableRaisingEvents = true;
                 watchers.Add(newWatcher);
             }
@@ -55,6 +64,19 @@ namespace SeekAndArchive
             {
                 RecursiveSearch(foundFiles, fileName, dir);
             }
+        }
+
+        private static void WatcherRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine("{0} renamed to {1}", e.OldFullPath, e.FullPath);
+            InitFilewatcher();
+        }
+
+        private static void WatcherDeleted(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Deleted)
+                Console.WriteLine("{0} has been deleted!", e.FullPath);
+                InitFilewatcher();
         }
 
         static void WatcherChanged(object sender, FileSystemEventArgs e)
