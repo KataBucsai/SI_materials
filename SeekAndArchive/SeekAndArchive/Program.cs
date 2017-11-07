9d0cc32c64f6a0038ff8c86ebe67d70a77279a89
KataBucsai <kata.kovacs@gmail.com>
2017-11-07 20:57:42 +0100

    implement archives

--- SeekAndArchive/SeekAndArchive/Program.cs
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,14 @@ namespace SeekAndArchive
         static List<FileSystemWatcher> watchers;
         static private string fileName;
         static private DirectoryInfo rootDir;
+        static List<DirectoryInfo> archiveDirs;
 
         static void Main(string[] args)
         {
             fileName = args[0];
             string directoryName = args[1];
             FoundFiles = new List<FileInfo>();
+            archiveDirs = new List<DirectoryInfo>();
             watchers = new List<FileSystemWatcher>();
 
             //examine if the given directory exists at all 
@@ -30,8 +33,11 @@ namespace SeekAndArchive
             }
 
             InitFilewatcher();
+            InitArchiveDirs();
 
+            Console.ReadLine();
         }
+
 
         private static void InitFilewatcher()
         {
@@ -45,7 +51,16 @@ namespace SeekAndArchive
                 Console.WriteLine("{0}", fil.FullName);
             }
 
-            Console.ReadLine();
+        }
+
+        private static void InitArchiveDirs()
+        {
+            archiveDirs.Clear();
+            //create archive directories
+            for (int i = 0; i < FoundFiles.Count; i++)
+            {
+                archiveDirs.Add(Directory.CreateDirectory("archive" + i.ToString()));
+            }
         }
 
         static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
@@ -70,20 +85,52 @@ namespace SeekAndArchive
         {
             Console.WriteLine("{0} renamed to {1}", e.OldFullPath, e.FullPath);
             InitFilewatcher();
+            InitArchiveDirs();
         }
 
         private static void WatcherDeleted(object sender, FileSystemEventArgs e)
         {
             if (e.ChangeType == WatcherChangeTypes.Deleted)
+            {
                 Console.WriteLine("{0} has been deleted!", e.FullPath);
                 InitFilewatcher();
+                InitArchiveDirs();
+            }
         }
 
         static void WatcherChanged(object sender, FileSystemEventArgs e)
         {
             if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
                 Console.WriteLine("{0} has been changed!", e.FullPath);
+                InitArchiveFile(sender);
+            }
         }
 
+        private static void InitArchiveFile(object sender)
+        {
+            //find the index of the changed file 
+            FileSystemWatcher senderWatcher = (FileSystemWatcher)sender;
+            int index = watchers.IndexOf(senderWatcher, 0);
+            //now that we have the index, we can archive the file 
+            ArchiveFile(archiveDirs[index], FoundFiles[index]);
+        }
+
+        static void ArchiveFile(DirectoryInfo archiveDir, FileInfo fileToArchive)
+        {
+            FileStream input = fileToArchive.OpenRead();
+            FileStream output = File.Create(archiveDir.FullName + @"\" + fileToArchive.Name + ".gz");
+            GZipStream Compressor = new GZipStream(output, CompressionMode.Compress);
+            int b = input.ReadByte();
+            while (b != -1)
+            {
+                Compressor.WriteByte((byte)b);
+
+                b = input.ReadByte();
+            }
+            Compressor.Close();
+            input.Close();
+            output.Close();
+        }
     }
 }
