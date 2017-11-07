0c2cea3c9e6aaf78161f0a8de302318fb297d096
KataBucsai <kata.kovacs@gmail.com>
2017-11-07 21:11:41 +0100

    implement isolated storage

---
+++ IsolatedStorageDemo/IsolatedStorageDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.IsolatedStorage;
+using System.IO;
+
+namespace IsolatedStorageDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User Prefs");
+            userWriter.Close();
+        }
+    }
+}
