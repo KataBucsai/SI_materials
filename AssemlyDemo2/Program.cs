0c8d9692e14b58a472eb07ee5c46f58a53c68522
KataBucsai <kata.kovacs@gmail.com>
2017-12-07 10:39:17 +0100

    implement 3. task

---
+++ AssemlyDemo2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemlyDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\System.ServiceProcess.dll";
+
+            // Using BindingFlags to only get declared and instance members
+            BindingFlags flags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance;
+
+            // Load the Assembly from the path
+            Assembly theAssembly = Assembly.LoadFrom(path);
+            Console.WriteLine(theAssembly.FullName);
+            Type[] types = theAssembly.GetTypes();
+
+            foreach (Type t in types)
+            {
+                Console.WriteLine(" Type: {0}", t.Name);
+                MemberInfo[] members = t.GetMembers(flags);
+
+                foreach (MemberInfo member in members)
+                {
+                    Console.WriteLine(" {0}: {1}", member.MemberType, member.Name);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
